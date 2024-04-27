dc80ffd1239f1ae0fc59e9b1e1a2342fcb84b8cb
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2024-04-26 18:03:20 -0700

    pushed TODOs

--- cloud_guardian/utils/tuple.py
+++ cloud_guardian/utils/tuple.py
@@ -27,3 +27,16 @@ permissions_set = set(unique_permissions)
 
 # Print the set of unique permissions
 print("\n".join(permissions_set))
+
+
+# TODO: Create the json structure as in "toy/example/processed
+# you can use source and destination columns as ids, 
+# permission FULL_CONTROL = * action
+# permission belongs => relates users to groups
+# permission sts:AssumeRole => relates roles 
+# etc..
+# you can use a custom utils/strings/get_name_and_type_from_id function to parse the source and destination
+# and get the type (role, group etc..) and an id (e.g. identity_1) etc..
+
+def create_jsons_from_tuple(data, output_folder):
+    """Create the json structure as in "toy/example/processed"""
